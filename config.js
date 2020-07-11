78702225abe6dccfd4d9937a65cd225a1036ad6b
Matt Dumler <mattd3v@pm.me>
2020-07-11 16:18:12 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    host: process.env.APP_DB_HOST,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD
+  },
+};
---
+++ db.js
@@ -0,0 +1,38 @@
+const config = require("./config");
+
+const { host, user, password } = config.db;
+console.log("ENV_CONFIG:", host, user, password);
+
+const db = require("knex")({
+  client: "pg",
+  connection: {
+    host: config.db.host,
+    user: config.db.user,
+    password: config.db.password
+  }
+});
+
+console.log(`Creating tables ...`);
+
+db.schema
+  .hasTable("users")
+  .then((exists) => {
+    if (!exists) {
+      console.log(`Creating table 'users'`);
+      return db.schema.createTable("users", (table) => {
+        table.increments();
+        table.string("email").notNullable();
+        table.string("firstname").notNullable();
+        table.timestamps(true, true);
+      });
+    }
+  })
+  .then(() => {
+    console.log(`Tables created succesfully`);
+    process.exit(0);
+  })
+  .catch(err => {
+    console.log(`Unable to init tables : ${err}`);
+    process.exit(1);
+  });
+
